8fdfc79e9c424decc1adc70d39f7a256c1a299b1
Fjolla2002 <fjolla.selimi17@gmail.com>
2024-10-29 20:48:58 +0100

    some changes

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './assets/style.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./assets/style.css";
+import "./assets/index.css";
 
-import router from './router'
+import router from "./router";
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
